b7f60a4b6979dd45eefca052fc1ca9cd5eccdfc2
Jullyana <jullyanacortes9@gmail.com>
2023-04-12 15:30:31 -0400

    Agregando un archivo

---
+++ Factorial.py
@@ -0,0 +1,24 @@
+#Comentario "Conociendo las variables de python"
+print("Hola mundo")
+#Variables tipo cadena
+Name= "Jullyana"
+Nombre= "Ruth"
+PI=3.14
+print (Name)
+print("Hola soy",Name)
+# Declarando variable de tipo numerico
+edad= 18
+#Impresion de texto + variable
+print (edad)
+print ("Mi edad es",edad)
+name="Sofia"
+name="Sofia"
+print ("Hola mi nombre es",name,"y tengo", edad, "años")
+#Actualizando variable
+edad=17
+name="Sofia"
+print("Hola mi nombre es", name,"y tengo",edad,"años")
+#Utilizando input
+nombre1= input ("¿Cual es tu nombre?\n")
+edad1= input ("¿Cual es tu edad?\n")
+print ("Tu nombre es", nombre1,"y tu edad es", edad1)
